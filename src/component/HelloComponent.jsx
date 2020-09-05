77d7c3353e5065e71e6361ed8b93b0d0ce5aeb66
andrijunaedi <andrijunaedi126@gmail.com>
2020-09-05 17:18:28 +0700

    Belajar : Stateless & StateFull Component

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () => (
+  <p className="text-p">Hello Functional Componet</p>
+);
+
+export default HelloComponent;
---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StateFullComponent extends React.PureComponent {
+  render() {
+    return <p>StateFull Component</p>;
+  }
+}
+
+export default StateFullComponent;

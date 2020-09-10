a2f588f54d79536f51ef3627d9ef0a44aeccbc4a
andrijunaedi <andrijunaedi126@gmail.com>
2020-09-10 21:17:52 +0700

    Learn : API call (POST)

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -8,6 +8,12 @@ class BlogPost extends Component {
     super(props);
     this.state = {
       posts: [],
+      formBlogPost: {
+        userId: 1,
+        id: 1,
+        title: '',
+        body: '',
+      },
     };
   }
 
@@ -16,11 +22,13 @@ class BlogPost extends Component {
   }
 
   getPostAPI = () => {
-    axios.get('http://localhost:5000/posts').then((result) => {
-      this.setState({
-        posts: result.data,
+    axios
+      .get('http://localhost:5000/posts?_sort=id&_order=desc')
+      .then((result) => {
+        this.setState({
+          posts: result.data,
+        });
       });
-    });
   };
 
   handleRemove = (id) => {
@@ -29,11 +37,56 @@ class BlogPost extends Component {
       .then(() => this.getPostAPI());
   };
 
+  handleFormChange = (event) => {
+    const { formBlogPost } = this.state;
+    const formBlogPostNew = { ...formBlogPost };
+    const timestamp = new Date().getTime();
+    formBlogPostNew.id = timestamp;
+    formBlogPostNew[event.target.name] = event.target.value;
+
+    this.setState({
+      formBlogPost: formBlogPostNew,
+    });
+  };
+
+  handleSave = () => {
+    const { formBlogPost } = this.state;
+    axios
+      .post('http://localhost:5000/posts', formBlogPost)
+      .then(() => this.getPostAPI())
+      .catch((error) => console.log(error));
+  };
+
   render() {
     const { posts } = this.state;
     return (
       <>
         <p className="section-title">Blog Post</p>
+        <div className="form-add-post">
+          <label htmlFor="title">
+            <p>Title</p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Add title"
+              onChange={this.handleFormChange}
+            />
+          </label>
+          <label htmlFor="body">
+            <p>Blog Content</p>
+            <textarea
+              name="body"
+              id="body"
+              cols="30"
+              rows="10"
+              placeholder="Add blog content"
+              onChange={this.handleFormChange}
+            />
+          </label>
+          <button type="submit" className="btn-save" onClick={this.handleSave}>
+            Save
+          </button>
+        </div>
         {posts.map((post) => (
           <Post
             key={post.id}
@@ -43,7 +96,6 @@ class BlogPost extends Component {
             remove={this.handleRemove}
           />
         ))}
-        <Post title="title" desc="desc" />
       </>
     );
   }

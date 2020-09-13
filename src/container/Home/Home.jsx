220a3fc5394c19a183015bcc6f041b7e8361c988
andrijunaedi <andrijunaedi126@gmail.com>
2020-09-14 00:31:44 +0700

    Learn : react-router-dom basic

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,14 +1,19 @@
 import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import BlogPost from '../BlogPost/BlogPost';
+import Product from '../Product/Product';
+import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
 
 class Home extends PureComponent {
   render() {
     return (
-      <div>
-        <p>Blog Post</p>
-        <hr />
-        <BlogPost />
-      </div>
+      <Router>
+        <>
+          <Route path="/" exact component={BlogPost} />
+          <Route path="/product" component={Product} />
+          <Route path="/lifecycle" component={LifeCycleComp} />
+        </>
+      </Router>
     );
   }
 }

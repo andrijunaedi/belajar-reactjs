89f5107df0bd6f5e27230a3065df043673b49038
andrijunaedi <andrijunaedi126@gmail.com>
2020-09-19 13:58:30 +0700

    Learn : react-hooks - side effect

--- src/container/pages/Hooks/Hooks.jsx
+++ src/container/pages/Hooks/Hooks.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Hooks.css';
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Title Change ${count}`;
+    return () => {
+      document.title = 'React App';
+    };
+  });
+
   return (
     <div className="p-hooks">
       <p>Nilai saat ini adalah : {count}</p>
